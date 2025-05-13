c8d6537498fd2aa1775d12181010981909055386
mysuf24 <mochammadyusuf.1008@gmail.com>
2025-05-13 09:16:53 +0700

    feat: add database and redis config

---
+++ config/config.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	"github.com/go-redis/redis/v8"
+)
+
+var DB *gorm.DB
+var RedisClient *redis.Client
+
+func InitDB() {
+	dsn := "host=localhost user=postgres password=12345 dbname=arsipdb port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	DB = db
+}
+
+func InitRedis() {
+	RedisClient = redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+	fmt.Println("Redis connected")
+}
